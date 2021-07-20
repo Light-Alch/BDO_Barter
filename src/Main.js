8224c305a777abfb6ece6038aa07f56da5e71863
Light-Alch <danny.jarquin.847@my.csun.edu>
2021-07-20 12:56:06 -0700

    Initial commit

---
+++ src/Main.js
@@ -0,0 +1,17 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>BDO Barter</h1>
+      <h2>To Do's:</h2>
+      <p>
+        Storage<br></br>
+        Central Market<br></br>
+        List of Nodes<br></br>
+        Lifeskills<br></br>
+        Settings<br></br>
+      </p>
+    </div>
+  );
+}
