67a6cec2279eaa35cdfcf5425e2892135fa8597a
Light-Alch <danny.jarquin.847@my.csun.edu>
2021-07-22 19:34:27 -0700

    test

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import { StrictMode } from "react";
 import ReactDOM from "react-dom";
-import App from "./Main";
+import App from "./App";
 //console.log(rose);
 const rootElement = document.getElementById("root");
 ReactDOM.render(
